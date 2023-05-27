4045859f0b035e95b19322250d4419ef95afa6b3
IvanFox913 <ivanfox913@hotmail.com>
2023-05-27 18:26:54 -0300

    Projeto iac dio infra v.1.0.

---
+++ src/fetchItems.js
@@ -0,0 +1,32 @@
+"use strict";
+
+const AWS = require("aws-sdk");
+
+const fetchItems = async (event) => {
+	
+	const dynamoDB = new AWS.DynamoDB.DocumentClient();
+	
+	let items;
+	
+	try {
+		
+		const results = awayt dynamorDB.scan({
+			TableName: "ItemTableNew"
+		}).promise();
+		
+		items = results.Items
+		
+	} catch (error) {
+		
+		console.log(error)
+	}
+	
+	return {
+		statusCode: 200,
+		body: JSON.stringify(items),
+	};		
+}
+
+module.exports = {
+	handler: fetchItems,
+};
